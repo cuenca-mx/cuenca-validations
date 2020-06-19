be153c9532c9f6091fc3b5293f481c33eaf4f6aa
Matin Tamizi <matin@users.noreply.github.com>
2020-06-19 12:57:32 -0500

    Better error card bin (#7)
    * improve the error in the case of a invalid card BIN
    
    * bump version

--- cuenca_validations/errors.py
+++ cuenca_validations/errors.py
@@ -3,4 +3,9 @@ from pydantic.errors import PydanticValueError
 
 class CardBinValidationError(PydanticValueError):
     code = 'payment_card_number.bin'
-    msg_template = 'card number does not have a valid Mexican debit card BIN'
+    msg_template = (
+        'The card number contains a BIN (first six digits) that does not have'
+        'a known association with a Mexican bank. To add the association,'
+        'please file an issue:'
+        'https://github.com/cuenca-mx/cuenca-validations/issues'
+    )
