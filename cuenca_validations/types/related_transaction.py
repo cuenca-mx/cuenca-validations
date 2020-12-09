ebdd36a0338f428a9f4f52a9ec4158a4451ddb2c
LuisBrime <luisbrimeg@gmail.com>
2020-12-08 19:19:52 -0600

    RelatedTransaction model now includes id as an attribute

--- cuenca_validations/types/related_transaction.py
+++ cuenca_validations/types/related_transaction.py
@@ -1,5 +1,5 @@
 import re
-from typing import Optional
+from typing import List, Optional
 
 mapper = dict(
     bill_payments='BillPayment',
@@ -17,7 +17,9 @@ class RelatedTransaction(str):
 
     def __init__(cls, uri: str):
         cls.uri = uri
-        cls.resource = cls._get_resource(uri)
+        resource_data = cls._get_resource(uri)
+        cls.resource = resource_data[0]
+        cls.id = resource_data[1]
 
     @classmethod
     def __get_validators__(cls):
@@ -31,11 +33,11 @@ class RelatedTransaction(str):
         return tr
 
     @staticmethod
-    def _get_resource(uri: str) -> Optional[str]:
-        match = re.search(r'/([a-z_]+)/[\w]+', uri)
+    def _get_resource(uri: str) -> List[Optional[str]]:
+        match = re.search(r'/([a-z_]+)/([\w]+)', uri)
         if not match:
-            return None
-        return match.group(1)
+            return [None, None]
+        return [match.group(1), match.group(2)]
 
     def get_model(cls):
         return mapper[cls.resource] if cls.resource in mapper else None
