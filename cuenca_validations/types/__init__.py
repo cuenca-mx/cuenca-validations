296e71819c65bbc90615b21c1381300a2c4c95ce
Ricardo Sánchez <rsc@cuenca.com>
2021-01-09 13:06:50 -0600

    Adding imports

--- cuenca_validations/types/__init__.py
+++ cuenca_validations/types/__init__.py
@@ -1,5 +1,6 @@
 __all__ = [
     'ApiKeyQuery',
+    'ApiKeyUpdateRequest',
     'BillPaymentQuery',
     'CardErrorType',
     'CardNetwork',
@@ -62,4 +63,8 @@ from .queries import (
     TransferQuery,
 )
 from .related_transaction import RelatedTransaction
-from .requests import StrictTransferRequest, TransferRequest
+from .requests import (
+    ApiKeyUpdateRequest,
+    StrictTransferRequest,
+    TransferRequest,
+)
