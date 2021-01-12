193fbc399e99700e09a19e1dccc70de59e77beec
Ricardo Sánchez <rsc@cuenca.com>
2021-01-12 16:11:15 -0600

    Lists are also valid

--- cuenca_validations/typing.py
+++ cuenca_validations/typing.py
@@ -1,5 +1,7 @@
-from typing import Any, Dict, MutableMapping, Optional, Union
+from typing import Any, Dict, List, MutableMapping, Optional, Union
 
-ClientRequestParams = Union[None, bytes, MutableMapping[str, str]]
+ClientRequestParams = Union[
+    None, bytes, MutableMapping[str, Union[str, List[str]]]
+]
 DictStrAny = Dict[str, Any]
 OptionalDict = Optional[Dict[str, Union[int, str]]]
