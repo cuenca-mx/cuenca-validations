fbce7b35b73623d20c2cd4bcd5928ba97cdbcd95
keryc Diaz <kerycdiaz@gmail.com>
2021-01-20 22:48:33 -0600

    fix year

--- tests/test_statement.py
+++ tests/test_statement.py
@@ -1,12 +1,16 @@
+import datetime as dt
+
 import pytest
 from pydantic import ValidationError
 
 from cuenca_validations.types import StatementQuery
 
+date_now = dt.date.today()
+
 
 def test_invalid_year():
     with pytest.raises(ValidationError) as exc_info:
-        StatementQuery(year=2022, month=1)
+        StatementQuery(year=date_now.year + 1, month=1)
     assert exc_info.value.errors()[0] == dict(
         loc=('month',),
         type='value_error',
@@ -16,7 +20,7 @@ def test_invalid_year():
 
 def test_invalid_year_month():
     with pytest.raises(ValidationError) as exc_info:
-        StatementQuery(year=2021, month=1)
+        StatementQuery(year=date_now.year, month=date_now.month)
     assert exc_info.value.errors()[0] == dict(
         loc=('month',),
         type='value_error',
