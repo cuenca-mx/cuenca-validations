ca994c7f9dc0fe86f5c836a8232682e13c1d588d
Ricardo Sánchez <rsc@cuenca.com>
2021-02-10 20:09:57 -0600

    Adding CardActivationRequest

--- cuenca_validations/types/requests.py
+++ cuenca_validations/types/requests.py
@@ -1,7 +1,7 @@
 from typing import Optional, Union
 
 from clabe import Clabe
-from pydantic import BaseModel, Extra, StrictStr
+from pydantic import BaseModel, Extra, StrictStr, conint, constr
 
 from ..types.enums import CardFundingType, CardIssuer, CardStatus
 from .card import PaymentCardNumber, StrictPaymentCardNumber
@@ -32,6 +32,17 @@ class CardUpdateRequest(BaseModel):
 class CardRequest(BaseModel):
     user_id: str
     ledger_account_id: str
+    issuer: CardIssuer
+    funding_type: CardFundingType
+
+
+class CardActivationRequest(BaseModel):
+    number: str
+    exp_month: conint(strict=True, ge=1, le=12)
+    exp_year: conint(strict=True, ge=2018, le=2099)
+    cvv2: constr(
+        strip_whitespace=True, strict=True, min_length=4, max_length=4
+    )
     issuer: CardIssuer
     funding_type: CardFundingType
 
