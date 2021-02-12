a09639bcfff6e27ac8b991f72cc6c307c7d4715a
Felipe López <flh.1989@gmail.com>
2021-02-11 20:08:51 -0600

    exclude_none=True

--- cuenca_validations/types/requests.py
+++ cuenca_validations/types/requests.py
@@ -9,7 +9,17 @@ from .card import PaymentCardNumber, StrictPaymentCardNumber
 from .general import StrictPositiveInt
 
 
-class TransferRequest(BaseModel):
+class BaseRequest(BaseModel):
+    class Config:
+        extra = Extra.forbid
+
+    def dict(self, *args, **kwargs) -> DictStrAny:
+        kwargs.setdefault('exclude_none', True)
+        kwargs.setdefault('exclude_unset', True)
+        return super().dict(*args, **kwargs)
+
+
+class TransferRequest(BaseRequest):
     recipient_name: StrictStr
     account_number: Union[Clabe, PaymentCardNumber]
     amount: StrictPositiveInt  # in centavos
@@ -21,20 +31,17 @@ class StrictTransferRequest(TransferRequest):
     account_number: Union[Clabe, StrictPaymentCardNumber]
 
 
-class CardUpdateRequest(BaseModel):
+class CardUpdateRequest(BaseRequest):
     user_id: Optional[str]
     ledger_account_id: Optional[str]
     status: Optional[CardStatus]
 
-    class Config:
-        extra = Extra.forbid
 
-
-class CardRequest(BaseModel):
+class CardRequest(BaseRequest):
     user_id: str
     ledger_account_id: str
 
 
-class ApiKeyUpdateRequest(BaseModel):
+class ApiKeyUpdateRequest(BaseRequest):
     user_id: Optional[str]
     metadata: Optional[DictStrAny]
