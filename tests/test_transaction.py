8cd6b1ca5bd20991992cec9a7cf47db8d11e15c3
keryc Diaz <kerycdiaz@gmail.com>
2021-02-15 13:11:24 -0600

    fix and tests

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,35 +1,43 @@
 import pytest
 from pydantic import BaseModel, ValidationError
 
-from cuenca_validations.types import RelatedTransaction
+from cuenca_validations.types import RelatedResource
 
 
 class Model(BaseModel):
-    related_transaction_uri: RelatedTransaction
+    related_uri: RelatedResource
+
+
+def test_related_account():
+    transaction_uri = '/accounts/BAXXX'
+    model = Model(related_uri=transaction_uri)
+    assert model.related_uri == transaction_uri
+    assert model.related_uri.get_model() == 'Account'
+    assert model.related_uri.id == 'BAXXX'
 
 
 def test_related_transaction():
     transaction_uri = '/deposits/SPXXX'
-    model = Model(related_transaction_uri=transaction_uri)
-    assert model.related_transaction_uri == transaction_uri
-    assert model.related_transaction_uri.get_model() == 'Deposit'
-    assert model.related_transaction_uri.id == 'SPXXX'
+    model = Model(related_uri=transaction_uri)
+    assert model.related_uri == transaction_uri
+    assert model.related_uri.get_model() == 'Deposit'
+    assert model.related_uri.id == 'SPXXX'
 
 
 def test_related_transaction_with_underscore():
     transaction_uri = '/bill_payments/STXXX'
-    model = Model(related_transaction_uri=transaction_uri)
-    assert model.related_transaction_uri == transaction_uri
-    assert model.related_transaction_uri.get_model() == 'BillPayment'
-    assert model.related_transaction_uri.id == 'STXXX'
+    model = Model(related_uri=transaction_uri)
+    assert model.related_uri == transaction_uri
+    assert model.related_uri.get_model() == 'BillPayment'
+    assert model.related_uri.id == 'STXXX'
 
 
 def test_invalid_uri_related_transaction():
     transaction_uri = '/depositsXXXXX'
     with pytest.raises(ValidationError) as exc_info:
-        Model(related_transaction_uri=transaction_uri)
+        Model(related_uri=transaction_uri)
     assert exc_info.value.errors()[0] == dict(
-        loc=('related_transaction_uri',),
+        loc=('related_uri',),
         type='value_error',
         msg='invalid uri format',
     )
