5d7c8f8385128c89dbe15898b47c605eacb4f4fc
Ricardo Sánchez <rsc@cuenca.com>
2021-03-01 17:15:06 -0600

    Addind AuthedException

--- cuenca_validations/errors.py
+++ cuenca_validations/errors.py
@@ -19,3 +19,36 @@ class CardBinValidationError(PydanticValueError):
 class NotDigitError(PydanticNotDigitError):
     code = 'digits'
     msg_template = 'value is not all digits'
+
+
+class AuthedException(Exception):
+    """Exceptions related to ApiKeys, Login, Password, etc"""
+
+
+class NoPasswordFound(AuthedException):
+    """User must create a password before to continue"""
+
+
+class UserNotLoggedIn(AuthedException):
+    """Login required for this method"""
+
+
+class WrongCreds(AuthedException):
+    """Invalid ApiKeys"""
+
+
+class MissingAuthorizationHeader(AuthedException):
+    """Neither Basic Auth or JWT found"""
+
+
+class AuthMethodNotAllowed(AuthedException):
+    """No permissions to use this authentication method"""
+
+
+AUTHED_ERROR_CODES = {
+    101: WrongCreds,
+    102: MissingAuthorizationHeader,
+    103: UserNotLoggedIn,
+    104: NoPasswordFound,
+    105: AuthMethodNotAllowed,
+}
--- cuenca_validations/version.py
+++ cuenca_validations/version.py
@@ -1,1 +1,1 @@
-__version__ = '0.7.7.dev6'
+__version__ = '0.7.7.dev7'
