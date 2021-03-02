bc9eb74c1a788e903fd9502542b16d6a72800d2e
Ricardo Sánchez <rsc@cuenca.com>
2021-03-02 12:36:24 -0600

    Status code and dicts

--- cuenca_validations/errors.py
+++ cuenca_validations/errors.py
@@ -23,29 +23,53 @@ class NotDigitError(PydanticNotDigitError):
 
 class AuthedException(Exception):
     """Exceptions related to ApiKeys, Login, Password, etc"""
+
     code: int
+    status_code: int
 
 
 class WrongCreds(AuthedException):
     """Invalid ApiKeys"""
+
     code = 101
+    status_code = 401
 
 
 class MissingAuthorizationHeader(AuthedException):
     """Neither Basic Auth or JWT found"""
+
     code = 102
+    status_code = 401
 
 
 class UserNotLoggedIn(AuthedException):
     """Login required for this method"""
+
     code = 103
+    status_code = 401
 
 
 class NoPasswordFound(AuthedException):
     """User must create a password before to continue"""
+
     code = 104
+    status_code = 401
 
 
 class AuthMethodNotAllowed(AuthedException):
     """No permissions to use this authentication method"""
+
     code = 105
+    status_code = 401
+
+
+AUTHED_ERROR_CODES = {
+    exc.code: exc
+    for exc in [
+        WrongCreds,
+        MissingAuthorizationHeader,
+        UserNotLoggedIn,
+        NoPasswordFound,
+        AuthMethodNotAllowed,
+    ]
+}
