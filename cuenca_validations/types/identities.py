ad7e76ccfffb8be253612a598037dde7770d7238
Manuel Avalos <manuelavalos97@gmail.com>
2021-11-16 11:44:39 -0600

    New changes

---
+++ cuenca_validations/types/identities.py
@@ -0,0 +1,50 @@
+import datetime as dt
+from typing import Optional
+
+from pydantic.dataclasses import dataclass
+
+from .enums import CardIssuerType, KYCFileType
+
+# pasar a rquests?
+
+
+@dataclass
+class TOSAgreement:
+    created_at: dt.datetime
+    version: int
+    ip: str
+    location: str
+    type: CardIssuerType
+
+
+@dataclass
+class Address:
+    created_at: dt.datetime
+    calle: str
+    numero_ext: str
+    numero_int: Optional[str]
+    codigo_postal: str
+    estado: str
+    ciudad: Optional[str]
+    colonia: str
+
+
+@dataclass
+class BlacklistValidation:
+    id: str
+    created_at: dt.datetime
+    feedme_uri: Optional[str]
+    value: Optional[str]
+    deactivated_at: Optional[dt.datetime]
+    status: str
+
+
+@dataclass
+class KYCFile:
+    created_at: dt.datetime
+    updated_at: dt.datetime
+    type: KYCFileType
+    feedme_uri_front: str
+    feedme_uri_back: str
+    is_mx: bool
+    data: Optional[str]
--- cuenca_validations/types/requests.py
+++ cuenca_validations/types/requests.py
@@ -19,25 +19,22 @@ from ..types.enums import (
     CardFundingType,
     CardholderVerificationMethod,
     CardIssuer,
-    CardIssuerType,
     CardPackaging,
     CardStatus,
     CardType,
     EcommerceIndicator,
-    GovtIdType,
     IssuerNetwork,
     PosCapability,
     SavingCategory,
     TrackDataMethod,
     TransactionTokenValidationStatus,
     UserCardNotification,
-    UserDataType,
-    UserProofType,
     WalletTransactionType,
 )
 from ..typing import DictStrAny
 from .card import PaymentCardNumber, StrictPaymentCardNumber
 from .general import StrictPositiveInt
+from .identities import Address, BlacklistValidation, KYCFile, TOSAgreement
 
 
 class BaseRequest(BaseModel):
@@ -262,92 +259,28 @@ class UserPldRiskLevelRequest(BaseModel):
     level: float = Field(ge=0.0, le=1.0)
 
 
-class AddressRequest(BaseModel):
-    user_id: Optional[str]
-    calle: str
-    numero_ext: str
-    codigo_postal: str
-    estado: str
-    colonia: str
-    ciudad: Optional[str] = None
-    numero_int: Optional[str] = None
-
-    class Config:
-        anystr_strip_whitespace = True
-        min_anystr_length = 1
-        extra = 'allow'
-
-
-class UserProofRequest(BaseModel):
-    user_id: Optional[str]
-    type: UserProofType
-    feedme_uri: Optional[str]
-    value: Optional[str]
-
-
-class UserDataRequest(BaseModel):
-    user_id: Optional[str]
-    type: UserDataType
-    data: str
-
-
-class BeneficiaryRequest(BaseModel):
-    user_id: Optional[str]
-    name: str
-    birth_date: dt.datetime
-    phone_number: str
-    user_relationship: str
-    percentage: int
-
-
-class GovtIDRequest(BaseModel):
-    user_uri: Optional[str]
-    type: GovtIdType
-    is_mx: bool
-    feedme_uri: str
-    number: Optional[str] = None
-
-
-class TOSAgreementRequest(BaseModel):
-    user_id: Optional[str]
-    version: int
-    ip: str
-    location: str
-    type: CardIssuerType
-
-
 class UserRequest(BaseModel):
+    # datos para crear identity
     nombres: str
     primer_apellido: str
     segundo_apellido: Optional[str] = None
+    curp: str
+    rfc: Optional[str] = None
     gender: Optional[str] = None
-    birth_place: Optional[str] = None
     birth_date: Optional[str] = None
+    birth_place: Optional[str] = None
     birth_country: Optional[str] = None
-    pronouns: Optional[str] = None
-    curp: Optional[str] = None
-
-    # estos son para en caso de que se mande de un jalón
-    address: Optional[AddressRequest] = None
-    phone_number: Optional[UserDataRequest] = None
-    email_address: Optional[UserDataRequest] = None
-    terms: Optional[TOSAgreementRequest] = None
-    profession: Optional[UserDataRequest] = None
-    proof_of_address: Optional[UserProofRequest] = None
-    proof_of_life: Optional[UserProofRequest] = None
-    govt_id: Optional[GovtIDRequest] = None
-
-    @validator(
-        'nombres', 'primer_apellido', 'gender', 'birth_place', 'birth_date'
-    )
-    def segundo_apellido_opcional(cls, segundo_value):
-        if len(segundo_value) <= 0:
-            raise ValueError('Length must be greater than 0')
-        return segundo_value
-
-    class Config:
-        anystr_strip_whitespace = True
-
-
-class HumanRequest(UserRequest):
-    user_id: Optional[str]
+    terms_of_service: TOSAgreement
+    blaclist_validation: BlacklistValidation
+    address: Address
+    govt_id: KYCFile
+    proof_of_address: KYCFile
+    proof_of_life: KYCFile
+    # para el user en sí
+    platform_id: str
+    phone_number: str
+    email_address: str
+    profession: str
+    platform_terms_of_service: TOSAgreement
+    status: str
+    level: str
