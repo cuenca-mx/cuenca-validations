20e63cd0f4ea5a95c04608d48f9c408cc11871a7
Miguel Acosta <migue.un.av@gmail.com>
2022-04-01 11:25:29 -0600

    Se cambia el nombre del valor to_pay_shipping a card_shipping

--- cuenca_validations/types/enums.py
+++ cuenca_validations/types/enums.py
@@ -88,7 +88,7 @@ class CommissionType(str, Enum):
     card_request = 'card_request'
     cash_deposit = 'cash_deposit'
     outgoing_spei = 'outgoing_spei'
-    to_pay_shipping = 'to_pay_shipping'
+    card_shipping = 'card_shipping'
 
 
 class EntryType(str, Enum):
