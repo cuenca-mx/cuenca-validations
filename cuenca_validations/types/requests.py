f7d751b6ec2c5b3d0fb221e9de799fe51fa494ad
Manuel Avalos <manuelavalos97@gmail.com>
2022-06-06 12:58:24 -0500

    v

--- cuenca_validations/types/requests.py
+++ cuenca_validations/types/requests.py
@@ -603,7 +603,7 @@ class KYCVerificationUpdateRequest(BaseRequest):
 class PlatformRequest(BaseModel):
     name: str
     rfc_curp: Optional[str]
-    establishment_date: Optional[str]
+    establishment_date: Optional[dt.date]
     country: Optional[Country]
     state: Optional[State]
     economic_activity: Optional[str]
