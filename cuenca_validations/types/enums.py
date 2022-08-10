b30ab4b428d5a0ce94e0c562aee819fc59c0ee7c
Andres Hernandez <andres@cuenca.com>
2022-08-10 11:54:42 -0500

    account_balance is better

--- cuenca_validations/types/enums.py
+++ cuenca_validations/types/enums.py
@@ -572,7 +572,7 @@ class UserStatus(str, Enum):
 class SessionType(str, Enum):
     registration = 'session.registration'
     show_card = 'session.show_card'
-    balance = 'session.balance'
+    account_balance = 'session.account_balance'
 
 
 class WebhookObject(str, Enum):
