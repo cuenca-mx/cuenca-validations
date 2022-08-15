c6238b09bc1d310ede827b30a9367038fab45df1
Andrés Hernández <57640881+andreshndz@users.noreply.github.com>
2022-08-15 12:10:54 -0500

    Balance Session (#279)
    * Balance Session
    
    * account_balance is better
    
    * Correct Version
    
    * Account entries

--- cuenca_validations/types/enums.py
+++ cuenca_validations/types/enums.py
@@ -572,6 +572,7 @@ class UserStatus(str, Enum):
 class SessionType(str, Enum):
     registration = 'session.registration'
     show_card = 'session.show_card'
+    account_balance = 'session.account_entries'
 
 
 class WebhookObject(str, Enum):
