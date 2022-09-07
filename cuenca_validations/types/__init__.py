14f851f1af958b208f3aaa1c93af11ca68775169
AlexViquez <alexviquez22@gmail.com>
2022-09-07 16:58:54 -0500

    ading to init

--- cuenca_validations/types/__init__.py
+++ cuenca_validations/types/__init__.py
@@ -5,6 +5,7 @@ __all__ = [
     'ApiKeyUpdateRequest',
     'AuthorizerTransaction',
     'BankAccountValidationRequest',
+    'BankAccountStatus',
     'BalanceEntryQuery',
     'BatchFileMetadata',
     'Beneficiary',
