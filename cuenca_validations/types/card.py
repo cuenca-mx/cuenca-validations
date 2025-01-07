325d61bef5ec3c20f05584ecefe57f734fc603ab
gabino <gabino@cuenca.com>
2025-01-07 11:27:20 -0600

    Refactor StrictPaymentCardNumber to extend Pydantic v2's PaymentCardNumber

--- cuenca_validations/types/card.py
+++ cuenca_validations/types/card.py
@@ -1,11 +1,10 @@
-from pydantic import BaseModel, field_validator
-from pydantic_core import PydanticCustomError
-from pydantic_extra_types.payment import PaymentCardBrand, PaymentCardNumber
+from pydantic_core import PydanticCustomError, core_schema
+from pydantic_extra_types.payment import PaymentCardNumber
 
 from ..card_bins import CARD_BINS
 
 
-class StrictPaymentCardNumber(BaseModel):
+class StrictPaymentCardNumber(PaymentCardNumber):
     """
     Refactored `StrictPaymentCardNumber` to leverage Pydantic v2's
     `PaymentCardNumber`, which now natively includes attributes such as
@@ -19,11 +18,12 @@ class StrictPaymentCardNumber(BaseModel):
     with a known Mexican bank.
     """
 
-    card_number: PaymentCardNumber
-
-    @field_validator('card_number')
-    def validate_bin(cls, card_number: PaymentCardNumber) -> PaymentCardNumber:
-        if card_number.bin not in CARD_BINS:
+    @classmethod
+    def validate(
+        cls, __input_value: str, _: core_schema.ValidationInfo
+    ) -> 'StrictPaymentCardNumber':
+        card = super().validate(__input_value, _)
+        if card.bin not in CARD_BINS:
             raise PydanticCustomError(
                 'payment_card_number.bin',
                 'The card number contains a BIN (first six digits) '
@@ -31,24 +31,8 @@ class StrictPaymentCardNumber(BaseModel):
                 'To add the association, please file an issue:'
                 'https://github.com/cuenca-mx/cuenca-validations/issues',
             )
-        return card_number
-
-    @property
-    def brand(self) -> PaymentCardBrand:
-        return self.card_number.brand
-
-    @property
-    def last4(self) -> str:
-        return self.card_number.last4
-
-    @property
-    def masked(self) -> str:
-        return self.card_number.masked
-
-    @property
-    def bin(self) -> str:
-        return self.card_number.bin
+        return cls(card)
 
     @property
     def bank_code(self) -> str:
-        return CARD_BINS[self.card_number.bin]
+        return CARD_BINS[self.bin]
--- tests/test_card.py
+++ tests/test_card.py
@@ -1,5 +1,5 @@
 import pytest
-from pydantic import ValidationError
+from pydantic import BaseModel, ValidationError
 from pydantic_extra_types.payment import PaymentCardBrand
 
 from cuenca_validations.types import StrictPaymentCardNumber
@@ -8,9 +8,13 @@ VALID_BBVA = '4772130000000003'
 INVALID_BIN = '4050000000000001'
 
 
+class CardModel(BaseModel):
+    card_number: StrictPaymentCardNumber
+
+
 def test_invalid_bin_strict_payment():
     with pytest.raises(ValidationError) as exc_info:
-        StrictPaymentCardNumber(card_number=INVALID_BIN)
+        CardModel(card_number=INVALID_BIN)
     print(exc_info.value)
     assert 'payment_card_number.bin' in str(exc_info.value)
     assert 'The card number contains a BIN (first six digits) ' in str(
@@ -19,9 +23,9 @@ def test_invalid_bin_strict_payment():
 
 
 def test_valid_bin_strict_payment():
-    card = StrictPaymentCardNumber(card_number=VALID_BBVA)
-    assert card.brand == PaymentCardBrand.visa
-    assert card.bin == '477213'
-    assert card.last4 == '0003'
-    assert card.masked == '477213******0003'
-    assert card.bank_code == '40012'
+    card = CardModel(card_number=VALID_BBVA)
+    assert card.card_number.brand == PaymentCardBrand.visa
+    assert card.card_number.bin == '477213'
+    assert card.card_number.last4 == '0003'
+    assert card.card_number.masked == '477213******0003'
+    assert card.card_number.bank_code == '40012'
