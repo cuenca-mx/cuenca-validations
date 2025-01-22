335980b778a68f9580f9bd0ffd339b3633b01cfb
gabino <gabino@cuenca.com>
2025-01-22 11:59:37 -0600

    Enhance uuid_field function to include type hint

--- cuenca_validations/types/helpers.py
+++ cuenca_validations/types/helpers.py
@@ -1,8 +1,9 @@
 import uuid
 from base64 import urlsafe_b64encode
+from typing import Callable
 
 
-def uuid_field(prefix: str = ''):
+def uuid_field(prefix: str = '') -> Callable[[], str]:
     def base64_uuid_func() -> str:
         return prefix + urlsafe_b64encode(uuid.uuid4().bytes).decode()[:-2]
 
