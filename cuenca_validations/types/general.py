aa0f8aee14ed8cb2e425ecd2b154e5f58e9704be
gabino <gabino@cuenca.com>
2025-01-27 12:43:40 -0600

    Refactor URL handling with HttpUrlString and AnyUrlString

--- cuenca_validations/types/general.py
+++ cuenca_validations/types/general.py
@@ -1,10 +1,18 @@
 import json
 from typing import Annotated, Any, Optional
 
-from pydantic import Field, StringConstraints
+from pydantic import AnyUrl, Field, HttpUrl, PlainSerializer, StringConstraints
 
 from ..validators import sanitize_dict, sanitize_item
 from .enums import State
+
+HttpUrlString = Annotated[
+    HttpUrl, PlainSerializer(lambda x: x.unicode_string(), return_type=str)
+]
+
+AnyUrlString = Annotated[
+    AnyUrl, PlainSerializer(lambda x: x.unicode_string(), return_type=str)
+]
 
 
 class SantizedDict(dict):
