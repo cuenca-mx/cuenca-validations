af5f4d194cb976c99b6f08de46081bb52c2549dd
gabino <gabino@cuenca.com>
2025-03-19 09:32:21 -0600

    Add 'tos' to KYCFileType and 'html' to FileExtension enums

--- cuenca_validations/types/enums.py
+++ cuenca_validations/types/enums.py
@@ -248,6 +248,7 @@ class KYCFileType(str, Enum):
     proof_of_address = 'proof_of_address'
     curp = 'curp'
     signature = 'signature'
+    tos = 'tos'
 
 
 class Gender(str, Enum):
@@ -648,6 +649,7 @@ class FileExtension(str, Enum):
     jpeg = 'jpeg'
     png = 'png'
     pdf = 'pdf'
+    html = 'html'
 
 
 class BankAccountStatus(str, Enum):
