95f8482bd51af94f62a809119da11131c14dd598
Gabino Morales <104923794+gmorales96@users.noreply.github.com>
2025-11-20 11:36:28 -0600

    Remove dash underscore from uuid (#393)
    * Enhance uuid_field function to sanitize output by replacing '-' and '_' with 'A' and 'B'
    
    * Fix test_sanitized_uuid to assert that '_' is not present in the generated UUID string
    
    * Update uuid_field function to replace '-' and '_' with random digits for enhanced uniqueness

--- cuenca_validations/types/helpers.py
+++ cuenca_validations/types/helpers.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import random
 import uuid
 from base64 import urlsafe_b64encode
 from typing import Callable, Optional, Union
@@ -12,7 +13,10 @@ from .identities import Curp
 
 def uuid_field(prefix: str = '') -> Callable[[], str]:
     def base64_uuid_func() -> str:
-        return prefix + urlsafe_b64encode(uuid.uuid4().bytes).decode()[:-2]
+        base64_str = urlsafe_b64encode(uuid.uuid4().bytes).decode()[:-2]
+        rnd = str(random.randint(0, 9))
+        sanitized = base64_str.replace('-', rnd).replace('_', rnd)
+        return prefix + sanitized
 
     return base64_uuid_func
 
