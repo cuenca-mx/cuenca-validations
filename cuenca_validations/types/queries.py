54ef5320835078812adf68db90d75bf7d1ce836e
Gabino Morales <104923794+gmorales96@users.noreply.github.com>
2026-09-15 13:02:03 -0600

    Add ClabeQuery and ReferencedClabeRequest for referenced CLABEs (#427)
    * Add ClabeQuery and ReferencedClabeRequest for referenced CLABEs
    
    * chore: bump version to 2.1.46
    
    * refactor: update ClabeQuery to use Union for allowed_curp_rfc type

--- cuenca_validations/types/queries.py
+++ cuenca_validations/types/queries.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from typing import Annotated, Optional
+from typing import Annotated, Optional, Union
 
 from clabe import Clabe
 from pydantic import (
@@ -28,7 +28,7 @@ from .enums import (
     UserStatus,
 )
 from .general import NormalizedName
-from .identities import Curp
+from .identities import Curp, Rfc
 
 MAX_PAGE_SIZE = 100
 
@@ -204,6 +204,10 @@ class AgentQuery(QueryParams):
 class FileQuery(QueryParams):
     type: Optional[KYCFileType] = None
     id: Optional[str] = None
+
+
+class ClabeQuery(QueryParams):
+    allowed_curp_rfc: Union[Curp, Rfc]
 
 
 class BankAccountValidationQuery(QueryParams):
